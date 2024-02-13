fd58e43b652953f26e6e92d64cf6f99d4afa5b53
saiteja1188 <loveui19@gmail.com>
2024-02-13 06:25:16 +0000

    SuperOver

---
+++ index.js
@@ -0,0 +1,9 @@
+const element = (
+  //   Write your code here.
+  <div className='bg-container'>
+    <img src='https://assets.ccbp.in/frontend/react-js/rcb-img.png' />
+    <img src='https://assets.ccbp.in/frontend/react-js/csk-img.png' />
+  </div>
+)
+
+ReactDOM.render(element, document.getElementById('root'))
